00e39eff7cfe0c0c37d28e71e301e4eb9251433d
Leticia <lettycuevas91@gmail.com>
2023-01-29 21:06:33 -0300

    solucion app.js

--- src/App.js
+++ src/App.js
@@ -5,17 +5,33 @@ import posts from './posts'
 // Modifica el componente App para implmentar la funcionalidad requerida
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      input: ''
+    }
+  }
+
+  onChangeHandler(event){
+   this.setState({
+      input: event.target.value,
+    })
+  }
   render() {
+    const lower = this.state.input.toLowerCase()
+    const list = posts
+        .filter(d => this.state.input === '' || d.title.toLowerCase().includes(lower))
+        .map((d, index) => <li key={index}>
+          <a href={d.url}><img src={d.image } alt={d.title}/></a>
+           <p>{ d.title }</p>
+          </li>);
     return (
       <div>
         <div className="filter">
-          <input type="text" placeholder="Ingresa el término de búsqueda" />
+          <input type="text" value={this.state.input} onChange={this.onChangeHandler.bind(this)} placeholder="Ingresa el término de búsqueda" />
         </div>
         <ul>
-          <li>
-            <a href={posts[0].url}><img src={posts[0].image } /></a>
-            <p>{ posts[0].title }</p>
-          </li>
+        {list}
         </ul>
       </div>
     )
